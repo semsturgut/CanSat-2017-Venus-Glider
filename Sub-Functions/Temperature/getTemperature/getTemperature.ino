553643ef5857de909f203b325f9fd6af57bdbfb8
eceeara <ecearaaa@gmail.com>
2017-04-10 22:40:47 +0300

    Add files via upload

---
+++ Sub-Functions/Temperature/getTemperature/getTemperature.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(9600);
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
+}
+  
+void loop() {
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+}
