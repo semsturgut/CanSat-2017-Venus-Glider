238c179be3c97be0255261fd4f86b6605c930b80
semsturgut <semsmustafaturgut@gmail.com>
2017-04-21 20:50:45 +0300

    Changing buzzer -> buzzerOn

---
+++ Sub-Functions/buzzerOn/buzzerOn.ino
@@ -0,0 +1,27 @@
+
+int buzzerPin = 12;
+void buzzerOn();
+void buzzerOff();
+
+void setup() {
+  Serial.begin(9600);
+  
+  }
+
+ void loop() {
+    buzzerOn();
+    delay (1000);
+    buzzerOff ();
+    delay (1000);
+   }
+
+ void buzzerOn () {
+  
+  tone(buzzerPin, 261); 
+  
+ }
+void buzzerOff () {
+
+  noTone (buzzerPin); 
+}
+
