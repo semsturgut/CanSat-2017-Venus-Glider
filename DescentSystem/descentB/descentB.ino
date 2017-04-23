56a5b0d8e5166df5730a09cd792c67cc35becdd5
eceeara <ecearaaa@gmail.com>
2017-04-23 18:14:23 +0300

    Add files via upload

---
+++ DescentSystem/descentB/descentB.ino
@@ -0,0 +1,28 @@
+float grav = 9.81;
+long previousMillis = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  descentB();
+}
+
+void descentB() {
+  float dist_X;// Container'dan ayrildigi anda ki yukseklik
+  float dist_Y = 390; //
+  float time_X = 3.00, time_Y; //Parasut acilana kadar gecen sure
+  //dist_X = pressure.altitude(P,baseline);
+  dist_X = 600.00;
+  time_Y = ((dist_X - (grav * pow(time_X, 2) / 2) - dist_Y) / 10.87)*1000;
+
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis > time_Y) {
+    previousMillis = currentMillis;
+    //setServo();
+    Serial.println("servoOpen();");
+  }
+
+}
+
