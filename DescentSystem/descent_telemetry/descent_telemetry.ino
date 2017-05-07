6ce296bd7e7cf54f5fd1615c123e8c110602af9c
semsturgut <semsmustafaturgut@gmail.com>
2017-05-07 20:26:55 +0300

    Added getCount and upCount funcs.

--- DescentSystem/descent_telemetry/descent_telemetry.ino
+++ DescentSystem/descent_telemetry/descent_telemetry.ino
@@ -18,6 +18,9 @@ void write(int, int);
 double getAltitude();
 double getTemperature();
 double getPressure();
+int check_light ();
+int getCount();
+void upCount(int);
 
 uint16_t startAddr = 0x0000;            // NV-RAM'de saklamak için başlangıç adresi
 uint16_t lastAddr;                      // NV-RAM'de saklamak için yeni adres
@@ -36,34 +39,35 @@ double baseline; // BMP sensor degerlerinin olcumu icin
 
 int sensorPin = A0; // select the input pin for LDR
 int ldr;
-int check_light ();
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(); // join i2c bus (address optional for master)
-  RTC.setRAM(0, (uint8_t *)&startAddr, sizeof(uint16_t));// Store startAddr in NV-RAM address 0x08
-  RTC.getRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
-  myservo.attach(9); // Servonun sinyal alacagi pin numarasini belirliyor.
-  pressure.begin();
-  baseline = getPressure();
-  check_Modules();
-  write(0x6B, 0); //Guc yonetimi registeri default:0
-  write(0x6A, 0); // I2C master kapali, acik olmasini istiyorsaniz 0x20 olmali
-  write(0x37, 0x02); //Bypass modu acik
+        Serial.begin(9600);
+        Wire.begin(); // join i2c bus (address optional for master)
+        RTC.setRAM(0, (uint8_t *)&startAddr, sizeof(uint16_t)); // Store startAddr in NV-RAM address 0x08
+        RTC.getRAM(54, (uint8_t *)&TimeIsSet, sizeof(uint16_t));
+        myservo.attach(9); // Servonun sinyal alacagi pin numarasini belirliyor.
+        pressure.begin();
+        baseline = getPressure();
+        check_Modules();
+        count = get_count();
+        write(0x6B, 0); //Guc yonetimi registeri default:0
+        write(0x6A, 0); // I2C master kapali, acik olmasini istiyorsaniz 0x20 olmali
+        write(0x37, 0x02); //Bypass modu acik
 }
 
 void loop() {
-  if (check_light()) {
-    count++;
-    con_data = String("4773") + ',' + String("CONTAINER") + ',' + String(getTime()) + ',' + String(count) + ',' + String(getAltitude()) + ',' + String(getTemperature()) + ',' + String(getVoltage()) + ',' + String("1");
-    Serial.println (con_data);
+        if (check_light()) {
+                count++;
+                con_data = String("4773") + ',' + String("CONTAINER") + ',' + String(getTime()) + ',' + String(count) + ',' + String(getAltitude()) + ',' + String(getTemperature()) + ',' + String(getVoltage()) + ',' + String("1");
+                Serial.println (con_data);
 
-    if (getAltitude() < 410) {
-      //servoOpen();
-    }
-    if (getAltitude() < 10) {
-      //buzzerOn ();
-    }
-    delay(1000);
-  }
+                if (getAltitude() < 410) {
+                        //servoOpen();
+                }
+                if (getAltitude() < 10) {
+                        //buzzerOn ();
+                }
+                up_count(count);
+                delay(1000);
+        }
 }
